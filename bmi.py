55debd2de8bfa3587cf2e623ad180b6d4d72ca96
he-cian-ning-yue <alan0901126755@gmail.com>
2026-09-12 15:57:28 +0800

    Add BMI categroy

--- bmi.py
+++ bmi.py
@@ -1,2 +1,13 @@
 def calculate_bmi(weight,height):
     return weight / (height ** 2)
+def bmi_category(bmi):
+    if bmi<18.5:
+        return '體重過輕'
+    elif bmi<24: 
+        return '正常體重'
+    elif bmi<27: 
+            return '體重過重'
+    else: 
+            return '過於肥胖'
+    
+    
